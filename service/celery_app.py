ac5a29641f4bc0eb0528ee91c9ac686b95c42fe3
Паша Лозинский <GeNeytly@yandex.ru>
2024-02-06 09:38:18 +0300

    Revert "test commit in feature branch"
    This reverts commit b452cf88e5d97fc35ae42157cb274a899587e267.

--- service/celery_app.py
+++ service/celery_app.py
@@ -1,25 +1,22 @@
 import os
-from datetime import timedelta
+import time
 
 from django.conf import settings
-from celery import Celery
 from django.utils import timezone
+from celery import Celery
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 app = Celery('service')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.broker_url = settings.CELERY_BROKER_URL
-app.conf.beat_schedule = {
-    'delete_expired_reports': {
-        'task': 'api.tasks.delete_expired_reports',
-        'schedule': timedelta(days=1)
-    },
-}
 app.conf.timezone = settings.CELERY_TIMEZONE
 app.autodiscover_tasks()
 
 
 @app.task()
 def debug_task():
+    time.sleep(5)
+    print('Hello from debug task!')
     print(timezone.now())
